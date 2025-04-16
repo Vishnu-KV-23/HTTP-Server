fba62f8eaa12539d423fc98c6f858aa0e5d5aaec
Vishnu-KV-23 <wishnukv2349@gmail.com>
2025-04-17 00:27:26 +0530

    sending status line and data line

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     #server_socket.accept() # wait for client
     server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    #byte literak needed to send data accross network, server_socket.accept() returns a tuple (client_socket, address) where client_socket is the socket object for the client and address is the address of the client. We use this object to use the sendall function
     #status line and header.. first crlf means end of status line and next crlf means end of header lines
 
 if __name__ == "__main__":
